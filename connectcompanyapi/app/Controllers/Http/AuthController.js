2c9d3b33bcc36b4b048f580a26eb411a7b83b507
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2021-10-24 21:17:34 -0500

    added the proper error message and also sending the error back to client

--- connectcompanyapi/app/Controllers/Http/AuthController.js
+++ connectcompanyapi/app/Controllers/Http/AuthController.js
@@ -1,31 +1,29 @@
 "use strict";
-const { validate } = use('Validator')
+const { validate } = use("Validator");
 
 const User = use("App/Models/User");
 class AuthController {
   async register({ request, response, auth }) {
     const data = request.post();
     try {
-
       const rules = {
-        email: 'required|email|unique:users,email',
-        code: 'required',
-        username:'required'
+        email: "required|email|unique:users,email",
+        code: "required",
+        username: "required",
       };
 
       const validation = await validate(request.post(), rules);
 
-
-    if (validation.fails()) {
-      return response.badRequest({
-        error: {
-          status: 401,
-          message:
-            "bad request, missing some required for internship properties",
-          fields: validation.messages(),
-        },
-      });
-    }
+      if (validation.fails()) {
+        return response.badRequest({
+          error: {
+            status: 401,
+            message:
+              "bad request, missing some required for internship properties",
+            fields: validation.messages(),
+          },
+        });
+      }
 
       await User.create({
         email: data.email,
@@ -33,17 +31,16 @@ class AuthController {
         username: data.username,
       });
       return response.status(200).json({
-        message: "Successfully registed in the data",
+        message: "Successfully registered in the data",
       });
     } catch (err) {
       return response.status(500).json({
-        message: err,
+        message: "Please enter all the fields and proper details",
+        err: err,
       });
     }
   }
   async login({ request, response }) {
-
-
     const userInfo = request.only(["email", "password"]);
 
     if (!userInfo.email || !userInfo.password) {
@@ -70,12 +67,10 @@ class AuthController {
 
     userInfo.code = decryptedData;
 
-
     const user = await User.query()
       .where("username", userinfo.username)
       .where("code", userinfo.code)
       .fetch();
-    
 
     if (user.rows.length > 0) {
       return response.status(200).json({
@@ -84,7 +79,8 @@ class AuthController {
       });
     } else {
       return response.status(500).json({
-        message: "unauthenticated user",
+        message: "Please provide the vaild details ",
+        err :err ,
       });
     }
   }
