e28d73bf4716b0fa470b8ecd1d2525c0bccd54ce
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2021-10-31 21:25:25 -0500

    resolved the issue in the login page and added the state management intial file - Saikiran Reddy Gangidi(S541902)

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import Router from "./router";
 import axios from "axios";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import store from "./store";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.prototype.$axios = axios.create({
   //baseURL: `${process.env.API_BASE_URL}`,
   baseURL: "https://connectcompanyapi.herokuapp.com/connectcompany/api/v1",
@@ -19,5 +21,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router: Router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
