9802ec8496b6f62d3cb8774225a3e262179b83ed
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2021-11-18 21:24:47 -0600

    updated the header with vue navbar where it shows the tabs and logout based by statemanagemenet and user details

--- client/src/components/Semantic/Header.vue
+++ client/src/components/Semantic/Header.vue
@@ -1,16 +1,66 @@
 <template>
-  <div class="header">
-    <h1>Connect Company</h1>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-navbar-brand href="#">ConnectCompany</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        
+      
+        <b-navbar-nav v-if="isloggied()" class="nav-but">
+          <b-nav-text 
+            ><router-link to="/login" class="border"
+              ><button>Logout</button></router-link
+            ></b-nav-text
+          >
+        </b-navbar-nav>
+        
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      login: false,
+      role:"",
+    };
+  },
+  mounted() {
+    this.login = this.$store.getters.loggedIn;
+   // this.role =this.$store.getters.userdetails.role;
+    //console.log(" loginned to ----", this.login,"------------------",this.role);
+  },
+  methods:{
+    // getName(){
+
+    //   return this.$store.getters.userdetails.role;
+    // },
+    isloggied(){
+      return this.$store.getters.loggedIn;
+    }
+  }
+  
+};
+</script>
 
 <style scoped>
-.header {
+b-navbar {
   padding: 10px;
-  text-align: center;
   background: #1abc9c;
   color: white;
   font-size: 50px;
+  float: left;
+  width: 100%;
 }
-
+.nav-but{
+  margin-right:10px;
+  padding:5px;
+  
+  text-decoration: none;
+}
 </style>
