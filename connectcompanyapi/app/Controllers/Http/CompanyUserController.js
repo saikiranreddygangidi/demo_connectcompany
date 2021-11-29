03534a103eb49f08c021d2b29a4bd177b98ddc37
vivekd31 <77740150+vivekd31@users.noreply.github.com>
2021-11-28 20:02:48 -0600

    Added route file for company user controller and added the API for getting all the company user details

--- connectcompanyapi/app/Controllers/Http/CompanyUserController.js
+++ connectcompanyapi/app/Controllers/Http/CompanyUserController.js
@@ -1,6 +1,27 @@
-'use strict'
+"use strict";
 
 class CompanyUserController {
+  async getAllEvents({ request, response, auth }) {
+    let events = [
+      {
+        id: 1,
+        eventName: "mobile launch",
+        eventType: "external",
+        eventDate: "11/7/2021",
+        eventLocation: "Maryville",
+        eventDescription: "Introducing brand new mobile",
+      },
+      {
+        id: 2,
+        eventName: "new recruitment",
+        eventType: "internal",
+        eventDate: "11/7/2021",
+        eventLocation: "Maryville",
+        eventDescription: "Introducing brand new mobile",
+      },
+    ];
+    return events;
+  }
 }
 
-module.exports = CompanyUserController
+module.exports = CompanyUserController;
