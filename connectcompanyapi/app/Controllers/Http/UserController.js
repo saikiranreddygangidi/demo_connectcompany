502c47cb2c5f299e058542f71d6f81b5bbb89cbe
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2021-12-03 22:08:39 -0600

    Added static data of companies in the UserController.js

--- connectcompanyapi/app/Controllers/Http/UserController.js
+++ connectcompanyapi/app/Controllers/Http/UserController.js
@@ -1,6 +1,13 @@
 'use strict'
 
 class UserController {
+    async getAllCompaniesByEmail({request,response,auth,params}){
+        const userName=params.userName;
+        const companies = [{id:1,companyName:"google",companyLocation:"United States",noofevents:1},
+        {id:2,companyName:"microsoft",companyLocation:"India",noofevents:2},
+     ];
+     return companies;
+    }
 }
 
 module.exports = UserController
